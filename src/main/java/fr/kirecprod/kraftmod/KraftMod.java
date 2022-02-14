13b8ad3da1abe2ed09c30e1b3304e37e27217ef9
kirecprod <kirecprod@gmail.com>
2022-02-14 22:09:01 +0100

    added: creative tab with endemonium logo

--- src/main/java/fr/kirecprod/kraftmod/KraftMod.java
+++ src/main/java/fr/kirecprod/kraftmod/KraftMod.java
@@ -2,6 +2,8 @@ package fr.kirecprod.kraftmod;
 
 import fr.kirecprod.kraftmod.core.init.BlockInit;
 import fr.kirecprod.kraftmod.core.init.ItemInit;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -9,7 +11,14 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(value = KraftMod.MODID)
 public class KraftMod {
 	public static final String MODID = "kraftmod";
-	
+
+	public static final CreativeModeTab TAB_KRAFTMOD = new CreativeModeTab(MODID) {
+		@Override
+		public ItemStack makeIcon() {
+			return ItemInit.ENDEMONIUM_INGOT.get().getDefaultInstance();
+		}
+	};
+
 	public KraftMod() {
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
--- src/main/java/fr/kirecprod/kraftmod/core/init/ItemInit.java
+++ src/main/java/fr/kirecprod/kraftmod/core/init/ItemInit.java
@@ -14,18 +14,17 @@ public final class  ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, KraftMod.MODID);
 
     //ITEMS
-    public static final RegistryObject<Item> SILVER_INGOT = ITEMS.register("silver_ingot", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).fireResistant()));
-    public static final RegistryObject<Item> TITANE_INGOT = ITEMS.register("titane_ingot", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).fireResistant()));
-    public static final RegistryObject<Item> PALADIUM_INGOT = ITEMS.register("paladium_ingot", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).fireResistant()));
-    public static final RegistryObject<Item> ENDEMONIUM_INGOT = ITEMS.register("endemonium_ingot", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).fireResistant()));
+    public static final RegistryObject<Item> SILVER_INGOT = ITEMS.register("silver_ingot", () -> new Item(new Item.Properties().tab(KraftMod.TAB_KRAFTMOD).fireResistant()));
+    public static final RegistryObject<Item> TITANE_INGOT = ITEMS.register("titane_ingot", () -> new Item(new Item.Properties().tab(KraftMod.TAB_KRAFTMOD).fireResistant()));
+    public static final RegistryObject<Item> PALADIUM_INGOT = ITEMS.register("paladium_ingot", () -> new Item(new Item.Properties().tab(KraftMod.TAB_KRAFTMOD).fireResistant()));
+    public static final RegistryObject<Item> ENDEMONIUM_INGOT = ITEMS.register("endemonium_ingot", () -> new Item(new Item.Properties().tab(KraftMod.TAB_KRAFTMOD).fireResistant()));
     //ITEMS
 
     //BLOCK ITEMS
-    public static final RegistryObject<BlockItem> SILVER_BLOCK_ITEM = ITEMS.register("silver_block", () -> new BlockItem(BlockInit.SILVER_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_MISC).fireResistant()));
-    public static final RegistryObject<BlockItem> TITANE_BLOCK_ITEM = ITEMS.register("titane_block", () -> new BlockItem(BlockInit.TITANE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_MISC).fireResistant()));
-    public static final RegistryObject<BlockItem> PALADIUM_BLOCK_ITEM = ITEMS.register("paladium_block", () -> new BlockItem(BlockInit.PALADIUM_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_MISC).fireResistant()));
-    public static final RegistryObject<BlockItem> ENDEMONIUM_BLOCK_ITEM = ITEMS.register("endemonium_block", () -> new BlockItem(BlockInit.ENDEMONIUM_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_MISC).fireResistant()));
-
+    public static final RegistryObject<BlockItem> SILVER_BLOCK_ITEM = ITEMS.register("silver_block", () -> new BlockItem(BlockInit.SILVER_BLOCK.get(), new Item.Properties().tab(KraftMod.TAB_KRAFTMOD).fireResistant()));
+    public static final RegistryObject<BlockItem> TITANE_BLOCK_ITEM = ITEMS.register("titane_block", () -> new BlockItem(BlockInit.TITANE_BLOCK.get(), new Item.Properties().tab(KraftMod.TAB_KRAFTMOD).fireResistant()));
+    public static final RegistryObject<BlockItem> PALADIUM_BLOCK_ITEM = ITEMS.register("paladium_block", () -> new BlockItem(BlockInit.PALADIUM_BLOCK.get(), new Item.Properties().tab(KraftMod.TAB_KRAFTMOD).fireResistant()));
+    public static final RegistryObject<BlockItem> ENDEMONIUM_BLOCK_ITEM = ITEMS.register("endemonium_block", () -> new BlockItem(BlockInit.ENDEMONIUM_BLOCK.get(), new Item.Properties().tab(KraftMod.TAB_KRAFTMOD).fireResistant()));
     //BLOCK ITEMS
 
 }
